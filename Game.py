02364e98c4b0b0b4a7a448e98e52939ba28abfa5
Nick Mills <carlos94563@gmail.com>
2015-05-18 14:09:39 -0400

    Create Game.py
    File to actually run the game

---
+++ Game.py
@@ -0,0 +1,12 @@
+#Nick Mills
+"""This code actually runs the game. To play the game, you run this file"""
+
+import Hjalmion
+
+def main():
+    Hjalmion.show_graphic()
+    Hjalmion.intro()
+    
+if __name__ == "__main__":
+    main()
+    
