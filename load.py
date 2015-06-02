f4abda4cec99021cb3c59cc74edb916ffd1cd7b3
Nick Mills <carlos94563@gmail.com>
2015-06-02 11:07:11 -0400

    Delete load.py
    Load is now handled in Hjalmion.py

--- load.py
+++
@@ -1,8 +0,0 @@
-#Nick Mills
-"""Does not currently work as I have no idea how to use pickle"""
-import Hjalmion
-import pickle
-
-def load_game(): #Loads a previously saved game from a file to be used as the current game state
-    pass
-
