dfef091def5914f563bcf9532359fa5c55022828
Nick Mills <carlos94563@gmail.com>
2015-06-02 11:07:29 -0400

    Delete save.py
    Save now handled in Hjalmion.py

--- save.py
+++
@@ -1,8 +0,0 @@
-#Nick Mills
-"""Does not currently work as I do not know how to use pickle"""
-import Hjalmion
-import pickle
-
-def save_game(): #Saves the current game state to be loaded from a file later
-    pass
-
